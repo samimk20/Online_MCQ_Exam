98b3b2d693a2f0c1a1167053c0915181790c71a7
samimk20 <133636444+samimk20@users.noreply.github.com>
2023-06-23 17:16:44 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -76,13 +76,7 @@ $helper = new TestHelper();
       <div class="container margin-50">
         <div class="row">
           <div class="col-md-12 col-sm-12">
-            <p>Aliquam enim enim, pharetra in sodales at, interdum sit amet dui. Nullam vulputate euismod urna non pharetra. Phasellus blandit mattis ipsum, ac laoreet Morlem lacinia et. Cras et ligula libero. 
-            Aliquam enim enim, pharetra in sodales at, interdum sit amet dui. Nullam vulputate euismod urna non pharetra. Phasellus blandit mattis ipsum, ac laoreet Morlem lacinia et. Cras et ligula libero. 
-            Aliquam enim enim, pharetra in sodales at, interdum sit amet dui. Nullam vulputate euismod urna non pharetra. Phasellus blandit mattis ipsum, ac laoreet Morlem lacinia et. Cras et ligula libero. </p>
-            
-            <p>Aliquam enim enim, pharetra in sodales at, interdum sit amet dui. Nullam vulputate euismod urna non pharetra. Phasellus blandit mattis ipsum, ac laoreet Morlem lacinia et. Cras et ligula libero. 
-            Aliquam enim enim, pharetra in sodales at, interdum sit amet dui. Nullam vulputate euismod urna non pharetra. Phasellus blandit mattis ipsum, ac laoreet Morlem lacinia et. Cras et ligula libero. 
-            Aliquam enim enim, pharetra in sodales at, interdum sit amet dui. Nullam vulputate euismod urna non pharetra. Phasellus blandit mattis ipsum, ac laoreet Morlem lacinia et. Cras et ligula libero. </p>
+            <p>The goal of this project is to build an "online MCQ exam." Through this project, staff members and administrators can conduct an online exam, answer exam questions, and view the result list. Users must first register before they can log in, take the exam, and receive their results right away. Full stack languages, such as HTML, CSS, JavaScript, PHP, MySQL, and others, will be used to construct this project.</p>
             </div>
           </div>
       </div>
@@ -100,26 +94,26 @@ $helper = new TestHelper();
           <section class="features-ico-left alt">
             <div class="col-md-3 col-sm-6">
               <div class="block margin-30"> <i class="fa ic-sm accent-color color-text fa-lg fa-star"></i>
-                <h5 class="short">Lorem Ipsum</h5>
-                <p>Aliquam enim enim, pharetra in sodales at, interdum sit amet dui. Nullam vulputate euismod urna non pharetra. Phasellus blandit mattis ipsum, ac laoreet Morlem lacinia et. Cras et ligula libero. </p>
+                <h5 class="short">Admin Login</h5>
+                <p>Here admin can login to the form using admin username and password. After logging in he can add the tests to conduct online exam, he can add or delete questions and answers to the particular test. He can allow user who wants to login to the form and also he can manage the result. </p>
               </div>
             </div>
             <div class="col-md-3 col-sm-6">
               <div class="block margin-30"> <i class="fa ic-sm accent-color color-text fa-lg fa-star"></i>
-                <h5 class="short">Lorem Ipsum</h5>
-                <p>Aliquam enim enim, pharetra in sodales at, interdum sit amet dui. Nullam vulputate euismod urna non pharetra. Phasellus blandit mattis ipsum, ac laoreet Morlem lacinia et. Cras et ligula libero. </p>
+                <h5 class="short">User Registration</h5>
+                <p>User can register himself by adding his information. User have to add detailed information with username and password, so he can easily login to the form. </p>
               </div>
             </div>
             <div class="col-md-3 col-sm-6">
               <div class="block margin-30"> <i class="fa ic-sm accent-color color-text fa-lg fa-star"></i>
-                <h5 class="short">Lorem Ipsum</h5>
-                <p>Aliquam enim enim, pharetra in sodales at, interdum sit amet dui. Nullam vulputate euismod urna non pharetra. Phasellus blandit mattis ipsum, ac laoreet Morlem lacinia et. Cras et ligula libero. </p>
+                <h5 class="short">User Login</h5>
+                <p>After registration user can login to the form using username and password which he had entered while registration. After logging in he is able to attend the test.</p>
               </div>
             </div>
             <div class="col-md-3 col-sm-6">
               <div class="block margin-30"> <i class="fa ic-sm accent-color color-text fa-lg fa-star"></i>
-                <h5 class="short">Lorem Ipsum</h5>
-                <p>Aliquam enim enim, pharetra in sodales at, interdum sit amet dui. Nullam vulputate euismod urna non pharetra. Phasellus blandit mattis ipsum, ac laoreet Morlem lacinia et. Cras et ligula libero. </p>
+                <h5 class="short">Submit Test</h5>
+                <p>After login user can enter into the test, and he can attend the test. After clearing all questions he can submit the test and get result quickly without any issues.</p>
               </div>
               
             </div>
